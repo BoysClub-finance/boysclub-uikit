097bbd1aa46449b94a126dda9621ef7d623ce7fa
boysclub-finance <boysclubfinance@gmail.com>
2021-03-02 17:31:58 +0200

    up

---
+++ src/widgets/Menu/icons/Logo.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Svg from "../../../components/Svg/Svg";
+import { SvgProps } from "../../../components/Svg/types";
+
+interface LogoProps extends SvgProps {
+  isDark: boolean;
+}
+
+const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
+  const textColor = isDark ? "#FFFFFF" : "#000000";
+  return (
+    <Svg viewBox="0 0 205 26" {...props}>
+      <image width="205" href={isDark ? "/images/pepe/LogoTextNewDark.png" : "/images/pepe/LogoTextNewWhite.png"} />
+    </Svg>
+  );
+};
+
+export default Logo;
